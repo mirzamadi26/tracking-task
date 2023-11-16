14a7bb646b408612df1338b8b98a8d0f74f86ac7
mirzamadi26 <mirzamadi026@gmail.com>
2023-11-16 14:47:05 +0500

    committing

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tracking_task/provider/provider.dart';
 import 'package:tracking_task/screens/home_page.dart';
-import 'package:tracking_task/screens/user_view.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => LocationProvider()),
-  ], child: const MyApp()));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,8 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tracking Task',
-      debugShowCheckedModeBanner: false,
-      home: UserScreen(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,9 +5,6 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-import '../constant.dart';
-import '../widgets/button.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
